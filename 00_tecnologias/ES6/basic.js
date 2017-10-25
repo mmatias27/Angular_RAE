859bc10c48d1aa9355bf4e8ac9c9c441a602cd37
Montserrat Matías <mmatias@rae.es>
2017-10-25 10:14:52 +0200

    Hola mundo

---
+++ 00_tecnologias/ES6/basic.js
@@ -0,0 +1,11 @@
+vardata = [{precio: 12}, {precio: 34}, {precio: 19}];
+data.forEach( elem =>{
+if(true) {
+constiva = 1.16
+letprecioFinal = elem.precio * iva
+console.log(`
+Oferta:
+El precio final es ${precioFinal}`);
+}
+// console.log (iva)
+});
